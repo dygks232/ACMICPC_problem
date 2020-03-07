001f7666cfec9bab5de02367e39b9db4086bc11e
noro <shqkdro@naver.com>
2020-03-07 18:07:48 +0900

    Create 12015_LongestIncreasingSubsequence2.cpp

---
+++ DP/LIS/12015_LongestIncreasingSubsequence2.cpp
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<algorithm>
+#include<cmath>
+#include<climits>
+#include<set>
+#include<map>
+#include<string>
+#include<cstring>
+#include<functional>
+using namespace std;
+
+#define MAXN 1000001
+
+void inputAndInit();
+void solve();
+void output();
+
+int N, answer;
+
+int arr[MAXN];
+vector<int> LIS;
+
+int main()
+{
+	inputAndInit();
+	solve();
+	output();
+	return 0;
+}
+
+void inputAndInit()
+{
+	scanf("%d", &N);
+	for (int i = 1; i <= N; i++)
+		scanf("%d", &arr[i]);
+	answer = INT_MIN;
+}
+
+void solve()
+{
+	vector<int>::iterator it;
+	for (int i = 1; i <= N; i++)
+	{
+		it = lower_bound(LIS.begin(), LIS.end(), arr[i]);
+		if (it == LIS.end())
+			LIS.push_back(arr[i]);
+		else
+			*it = arr[i];
+	}
+	answer = LIS.size();
+}
+
+void output()
+{
+	printf("%d\n", answer);
+}
